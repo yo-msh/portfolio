d7426853651c41e1437fd98c95c6c9d43b4b218e
manvardhan singh <manvardhansingh05@gmail.com>
2024-11-20 10:54:01 +0530

    added sic-2-pass project

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,6 +12,17 @@ const data = [
       + 'Successfully deployed the application on Render ',
   },
   {
+    title: 'SIC Assembler',
+    subtitle: 'October 2024',
+    link: 'https://github.com/yo-msh/SIC-2-Pass-Assembler.git',
+    image: '/images/projects/sicAssembler.png',
+    date : '2024-10-25',
+    desc:
+      'Developed a two-pass assembler for the Simplified Instructional Computer (SIC) architecture. '
+      + 'Implemented lexical analysis, syntax analysis, and code generation to convert SIC assembly code to machine code. '
+      + 'Designed a user-friendly interface for assembling and executing SIC programs.',
+  },
+  {
     title: 'Stock Portfolio Risk Management',
     subtitle: 'March 2024',
     link: 'https://github.com/yo-msh/stock-portfolio-risk-management',
